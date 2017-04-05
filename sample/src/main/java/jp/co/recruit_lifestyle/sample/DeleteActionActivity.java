f6f59f6c2ddbf5bda3661e7f204abc901103d5d9
YoshihideSogawa <y_sogawa@RLSUU054602M02.local>
2017-04-05 18:02:25 +0900

    Update Sample - Use stopService instead of bindService

--- sample/src/main/java/jp/co/recruit_lifestyle/sample/DeleteActionActivity.java
+++ sample/src/main/java/jp/co/recruit_lifestyle/sample/DeleteActionActivity.java
@@ -2,37 +2,20 @@ package jp.co.recruit_lifestyle.sample;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
-import android.app.Service;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
 
 import jp.co.recruit.floatingview.R;
 import jp.co.recruit_lifestyle.sample.fragment.DeleteActionFragment;
-import jp.co.recruit_lifestyle.sample.service.CustomFloatingViewService;
 
 /**
  * 通知から起動後に削除アクションを行う画面です。
  */
-public class DeleteActionActivity extends Activity implements ServiceConnection, DeleteActionFragment.DeleteActionCallback {
-
-    /**
-     * デバッグログ用のタグ
-     */
-    private static final String TAG = "DeleteActionActivity";
+public class DeleteActionActivity extends Activity {
 
     /**
      * 設定フラグメントのタグ
      */
     private static final String FRAGMENT_TAG_DELETE_ACTION = "delete_action";
-
-    /**
-     * 削除対象のService
-     */
-    private Service mTargetService;
 
     /**
      * {@inheritDoc}
@@ -48,35 +31,5 @@ public class DeleteActionActivity extends Activity implements ServiceConnection,
             ft.commit();
         }
 
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void onServiceConnected(ComponentName name, IBinder service) {
-        mTargetService = ((CustomFloatingViewService.CustomFloatingViewServiceBinder) service).getService();
-
-        // バインド直後に即切り
-        if (mTargetService != null) {
-            unbindService(this);
-            mTargetService.stopSelf();
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void onServiceDisconnected(ComponentName name) {
-        mTargetService = null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void clearFloatingView() {
-        bindService(new Intent(this, CustomFloatingViewService.class), this, Context.BIND_AUTO_CREATE);
     }
 }
